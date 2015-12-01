3866898fc38023ee6abc4e680809d81925dfd523
Sertan Senturk <contact@sertansenturk.com>
2015-12-01 07:03:45 +0100

    Added txt section validation

--- extras/unittests/validatetxtcontent.py
+++ extras/unittests/validatetxtcontent.py
@@ -1,4 +1,18 @@
 from symbtrdataextractor import extractor
+from fileoperations.fileoperations import getFileNamesInDir
 
-def dummy_test():
-	pass
+import os
+
+def test_txt_data():
+	[txtfilepaths, txtfolders, txtnames] = get_txt_filenames()
+
+	isAllDataValid = True
+	for tf, tn in zip(txtfilepaths, txtnames):
+		txtdata, isDataValid = extractor.extract(tf)
+		isAllDataValid = isDataValid if isAllDataValid else False
+
+	assert(isAllDataValid)
+
+def get_txt_filenames():
+	symbTrTxtfolder = './txt/'
+	return getFileNamesInDir(symbTrTxtfolder, keyword = '*.txt')
