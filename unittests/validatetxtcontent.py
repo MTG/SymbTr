4a6e81f02820903c98f229cab107a0cab113588d
Sertan Senturk <contact@sertansenturk.com>
2016-04-08 06:00:17 +0200

    Minor changes in unittests

--- unittests/validatetxtcontent.py
+++ unittests/validatetxtcontent.py
@@ -3,12 +3,12 @@ from fileoperations.fileoperations import get_filenames_in_dir
 
 
 def test_txt_data():
+    extractor = SymbTrDataExtractor(print_warnings=False)
     [txtfilepaths, txtfolders, txtnames] = get_txt_filenames()
 
     is_all_txt_data_valid = True
     for tf, tn in zip(txtfilepaths, txtnames):
         try:
-            extractor = SymbTrDataExtractor(print_warnings=False)
             txtdata, is_txt_data_valid = extractor.extract(tf)
             is_all_txt_data_valid = is_txt_data_valid \
                 if is_all_txt_data_valid else False
