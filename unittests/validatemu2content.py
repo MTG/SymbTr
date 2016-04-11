f727718e66512cae5df85bb275647ed9d8e89927
Sertan Senturk <contact@sertansenturk.com>
2016-04-11 05:56:33 +0200

    Added unicode error to mu2 unittests

--- unittests/validatemu2content.py
+++ unittests/validatemu2content.py
@@ -8,6 +8,7 @@ def test_mu2_header():
                  6: u'Çek', 7: u'Söz-1', 8: u'Söz-2'}
 
     num_columns = 10
+    all_read_correctly = True
     all_headers_valid = True
     all_header_rows_valid = True
     all_num_columns_correct = True
@@ -15,40 +16,50 @@ def test_mu2_header():
     [mu2filepaths, mu2folders, mu2names] = get_mu2_filenames()
 
     for mf, mn in zip(mu2filepaths, mu2names):
-        mu2_header, header_row, is_header_valid = Mu2Reader.read_header(
-            mf, symbtr_name=mn)
+        try:
+            mu2_header, header_row, is_header_valid = Mu2Reader.read_header(
+                mf, symbtr_name=mn)
 
-        if not is_header_valid:
-            all_headers_valid = False
+            if not is_header_valid:
+                all_headers_valid = False
 
-        if not len(header_row) == num_columns:
-            all_num_columns_correct = False
-            print mn + ': Number of columns is different than 10!'
+            if not len(header_row) == num_columns:
+                all_num_columns_correct = False
+                print mn + ': Number of columns is different than 10!'
 
-        for ii in range(0, len(col_names) + 3):
-            if ii in [0, 1]:
-                try:
-                    dummyint = int(header_row[ii])
-                except ValueError:  # not int
-                    all_header_rows_valid = False
-                    print(mn + ': ' + str(ii) + 'th column in the header ' +
-                          'row should have been an integer!')
-            elif ii == len(col_names) + 2:
-                try:
-                    dummyfloat = float(header_row[ii])
-                except ValueError:  # not float
-                    all_header_rows_valid = False
-                    print(mn + ': ' + str(ii) + 'th column in the header ' +
-                          'row should have been a float!')
-            else:
-                if not header_row[ii] == col_names[ii]:
-                    all_header_rows_valid = False
-                    print(u'%s: %dth column in the header row should have '
-                          u'been named "%s" instead of "%s"'
-                          % (mn, ii, col_names[ii], header_row[ii]))
+            for ii in range(0, len(col_names) + 3):
+                if ii in [0, 1]:
+                    try:
+                        dummyint = int(header_row[ii])
+                    except ValueError:  # not int
+                        all_header_rows_valid = False
+                        print(
+                        mn + ': ' + str(ii) + 'th column in the header ' +
+                        'row should have been an integer!')
+                elif ii == len(col_names) + 2:
+                    try:
+                        dummyfloat = float(header_row[ii])
+                    except ValueError:  # not float
+                        all_header_rows_valid = False
+                        print(
+                        mn + ': ' + str(ii) + 'th column in the header ' +
+                        'row should have been a float!')
+                else:
+                    if not header_row[ii] == col_names[ii]:
+                        all_header_rows_valid = False
+                        print(u'%s: %dth column in the header row should have '
+                              u'been named "%s" instead of "%s"'
+                              % (mn, ii, col_names[ii], header_row[ii]))
 
-    assert (all_header_rows_valid and all_num_columns_correct and
-            all_headers_valid)
+        except UnicodeEncodeError:
+            all_read_correctly = False
+            print(mn + ' unicode read error')
+
+
+    assert all_read_correctly, 'Unicode errors'
+    assert all_header_rows_valid, 'Invalid header rows'
+    assert all_num_columns_correct, 'Incorrect number of columns'
+    assert all_headers_valid, 'Invalid header values'
 
 
 def get_mu2_filenames():
